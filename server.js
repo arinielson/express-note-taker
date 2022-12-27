268db5b02436b2e3459e67773b1feec5cabe8927
Ari N <an-nielson@hotmail.com>
2022-12-26 19:20:20 -0700

    edits

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.get('/notes', (req, res) =>
 );
 
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Note Taker app listening at http://localhost:${PORT}`)
 );
