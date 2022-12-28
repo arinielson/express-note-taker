6d787ae8ea4636f2910c2ea93b0cdfda05c6e4ca
Ari N <an-nielson@hotmail.com>
2022-12-27 21:24:59 -0700

    remove files & server edits

--- server.js
+++ server.js
@@ -1,65 +1,67 @@
-const express = require('express');
+const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const cors = require("cors");
-const path = require('path');
 
-const {
-  getNotesHTML,
-  getIndexHTML,
-} = require("./controllers/html");
-const getNotesFromDatabase = require("./controllers/api");
+const path = require("path");
 
 const util = require("util");
 
 const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
 
-const PORT = process.env.PORT || 3001;
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', getIndexHTML);
+app.get('/', (req, res) => 
+  res.send('Navigate to /notes')
+);
 
-app.get('/notes', getNotesHTML);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
-app.get('api/notes', async (req, res) => {
+app.get("/api/notes", async (req, res) => {  
   try {
-    const notes = await readFileAsync('./db/db.json', 'utf-8');
+    const notes = await readFileAsync("./db/db.json", "utf-8");
     res.json(JSON.parse(notes));
-  } catch (err){
-    throw err;
-  }  
+  } catch (error) {
+    throw error;
+  }
 });
 
-app.post('api/notes', async (req, res) => {
+app.post("/api/notes", async (req, res) => {  
   try {
-    const notes = await readFileAsync('./db/db.json', 'utf-8');
-
+    const notes = await readFileAsync("./db/db.json", "utf-8");
     const newNote = req.body;
-    const newNoteId = uuidv4();
+    
+    const newNoteID = uuidv4();
     const newNoteData = {
-      id: newNoteId,
+      
+      id: newNoteID,
       title: newNote.title,
       text: newNote.text,
     };
-    
     const parseNote = JSON.parse(notes);
-
+    
     parseNote.push(newNoteData);
     res.json(newNoteData);
 
-    await writeFileAsync('./db/db.json', JSON.stringify(parseNote));
-  } catch (err){
-    throw err;
-  }  
+    
+    await writeFileAsync("./db/db.json", JSON.stringify(parseNote));
+    
+  } catch (error) {
+    throw error;
+  }
 });
 
-app.listen(PORT, () =>
-  console.log(`Note Taker app listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Note taker app listening at http://localhost:${PORT}`);
+});
