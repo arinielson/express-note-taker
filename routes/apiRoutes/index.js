98915f4cfd0e95b3d5ff80d5fc866ab69c8fb462
Ari N <an-nielson@hotmail.com>
2023-01-01 03:43:44 -0700

    multiple edits

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const {createNewNote, updateDb} = require("../../lib/notes");
+const {createNewNote, updateDb} = require("../../notes/notes");
 const { v4: uuidv4 } = require('uuid');
 const {notes} = require("../../db/db.json");
 
-// show all notes in json data
 router.get("/notes", (req, res) => {
     let results = notes;
     res.json(results);
