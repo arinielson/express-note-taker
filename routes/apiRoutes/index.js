9951e6fe6ed64ca24f702f390ccf7535b7f22b79
Ari N <an-nielson@hotmail.com>
2023-01-01 03:54:09 -0700

    final edits

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const {createNewNote, updateDb} = require("../../notes/notes");
+const {createNewNote, updateDb} = require("../../lib/notes");
 const { v4: uuidv4 } = require('uuid');
 const {notes} = require("../../db/db.json");
 
+// show all notes in json data
 router.get("/notes", (req, res) => {
     let results = notes;
     res.json(results);
--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
+// parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
 app.use(express.json());
 
+// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Note taker app listening at http://localhost:${PORT}`);
-});
+    console.log(`API server now on port ${PORT}. Welcome!`);
+  });
